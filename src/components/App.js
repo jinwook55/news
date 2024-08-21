020b8734ef24ec5cbaadf9f307136487a06b3e84
jinwook55 <jinwook55@gmail.com>
2024-08-21 19:19:24 +0900

    news

---
+++ src/components/App.js
@@ -0,0 +1,106 @@
+import { useEffect, useState } from "react";
+import NewsList from "./NewsList";
+import NewsForm from "./NewsForm";
+import { getNews, createNews, updateNews, deleteNews } from "../api";
+
+const LIMIT = 6;
+
+function App() {
+  const [order, setOrder] = useState("aid");
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
+
+  const [items, setItems] = useState([]);
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+  const [isLoading, setIsLoading] = useState(false); // 로딩 처리
+  const [loadingError, setLoadingError] = useState(null); //네트웍 에러 처리
+
+  const handleNewestClick = () => setOrder("aid");
+  const handleBestClick = () => setOrder("rating");
+  const handleDelete = async (aid) => {
+    try {
+      // filter로 뉴스 아이템 삭제하기
+      const result = await deleteNews(aid);
+      if (!result) return;
+      const nextItems = items.filter((item) => item.aid !== aid);
+      setItems(nextItems);
+    } catch (error) {
+      console.error("Error deleting news", error);
+    }
+  };
+
+  const handleLoad = async (options) => {
+    // 서버에서 정렬한 데이터 받아오기
+    let result;
+    try {
+      setLoadingError(null);
+      setIsLoading(true); // 진행중 시작
+      result = await getNews(options);
+    } catch (error) {
+      setLoadingError(error);
+      return;
+    } finally {
+      setIsLoading(false); // 진행중 종료
+    }
+
+    const { paging, news } = result;
+
+    if (options.offset === 0) {
+      setItems(news);
+    } else {
+      setItems((prevItems) => [...prevItems, ...news]);
+    }
+    setOffset(options.offset + options.limit); // 상쇄페이지 카운트 스테이트
+    let parsePaging = JSON.parse(paging);
+    setHasNext(parsePaging.hasNext);
+  };
+
+  // 더보기 버튼 핸들러
+  const handleLoadMore = async () => {
+    await handleLoad({ order, offset, limit: LIMIT });
+  };
+
+  const handleCreateSuccess = (news) => {
+    setItems((prevItems) => [news, ...prevItems]);
+  };
+
+  const handleUpdateSuccess = (news) => {
+    setItems((prevItems) => {
+      const splitIdx = prevItems.findIndex((item) => item.aid === news.aid);
+      return [
+        ...prevItems.slice(0, splitIdx),
+        news,
+        ...prevItems.slice(splitIdx + 1),
+      ];
+    });
+  };
+
+  // 마운트 시점과  정렬컬럼이 바뀔때 비동기 통신 요청
+  useEffect(() => {
+    handleLoad({ order, offset: 0, limit: LIMIT });
+  }, [order]);
+
+  return (
+    <div>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+      <NewsForm onSubmit={createNews} onSubmitSuccess={handleCreateSuccess} />
+      <NewsList
+        items={sortedItems}
+        onDelete={handleDelete}
+        onUpdate={updateNews}
+        onUpdateSuccess={handleUpdateSuccess}
+      />
+      {hasNext && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더보기
+        </button>
+      )}
+      {loadingError?.message && <span>{loadingError.message}</span>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NewsList.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import Rating from "./Rating";
+import NewsForm from "./NewsForm";
+import "./NewsList.css";
+
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
+function NewsListItem({ item, onDelete, onEdit }) {
+  const handleDeleteClick = () => {
+    onDelete(item.aid);
+  };
+  const { img, title, rating, date, content } = item;
+  const handleEditClick = () => {
+    onEdit(item.aid);
+  };
+  return (
+    <div className="NewsListItem">
+      <img className="NewsListItem-img" src={img} alt={title} />
+      <div>
+        <h1>{title}</h1>
+        <Rating value={rating} />
+        <p>{formatDate(date)}</p>
+        <p>{content}</p>
+        <button onClick={handleEditClick}>수정</button>
+        <button onClick={handleDeleteClick}>삭제</button>
+      </div>
+    </div>
+  );
+}
+
+function NewsList({ items, onUpdate, onUpdateSuccess, onDelete }) {
+  const [editingId, setEditingId] = useState(null);
+  const handleCancel = () => setEditingId(null);
+  return (
+    <ul className="NewsList">
+      {items.map((item) => {
+        if (item.aid === editingId) {
+          const { aid, img, title, rating, content } = item;
+          const initialValues = { title, rating, content, imgFile: null };
+
+          const handleSubmit = (formData) => onUpdate(aid, formData);
+
+          const handleSubmitSuccess = (news) => {
+            onUpdateSuccess(news);
+            setEditingId(null);
+          };
+
+          return (
+            <li key={item.aid}>
+              <NewsForm
+                initialValues={initialValues}
+                initialPreview={img}
+                onCancel={handleCancel}
+                onSubmitSuccess={handleSubmitSuccess}
+                onSubmit={handleSubmit}
+              />
+            </li>
+          );
+        }
+        return (
+          <li key={item.aid}>
+            <NewsListItem
+              item={item}
+              onDelete={onDelete}
+              onEdit={setEditingId}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default NewsList;
